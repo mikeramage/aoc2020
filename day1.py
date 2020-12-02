bf70b0c5bb66ed530cf169a858ba1dcf66d39f8b
mikeramage <mike.ramage@gmail.com>
2020-12-02 00:50:35 +0000

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,16 @@
+entries = list()
+with open("day1_expenses.txt", "r") as f:
+    for line in f:
+        entries.append(int(line.strip()))
+
+for i in range(len(entries)):
+    for j in range(i+1, len(entries)):
+        if (entries[i] + entries[j] == 2020):
+            print(f"{entries[i]}, {entries[j]}: {entries[i] * entries[j]}")
+
+for i in range(len(entries)):
+    for j in range(i+1, len(entries)):
+        for k in range(j+1, len(entries)):
+            if (entries[i] + entries[j] + entries[k] == 2020):
+                print(f"{entries[i]}, {entries[j]}, {entries[k]}: {entries[i] * entries[j] * entries[k]}")
+
